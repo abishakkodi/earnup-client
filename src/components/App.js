52dca7bb770218df44009796b05805ed597ac75f
Abishak Kodi <abishak.kodi@gmail.com>
2020-02-08 17:58:42 -0800

    Chat functionality rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../logo.svg';
 import Main from './Main';
 import '../App.css';
- 
+import socket from '../utils/socket';
 
-const App = () => {
-  return (
-    <div className="App">
-      
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: []
+    }
+  }
+
+  componentDidMount(){
+    socket.on('chat-message', (msg) => {
+      console.log('C DID MOUNT SOCKET FIRE');
+      let { messages } = this.state;
+      messages.push(msg);
+      this.setState({ messages});
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
         <Main />
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            {this.state.messages.map((msg,i) => {
+              return (<div key={i}>
+                <h3>{msg}
+                  </h3> </div>)
+            })}
+          </div> 
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
